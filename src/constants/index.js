9be19ba98446de2fc4e00dd541b066bc8826a164
dilrong <wet932@naver.com>
2021-03-17 18:48:42 +0900

    Add: Check box

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const collatedTasks = [
-    {key: 'INBOX', name: 'Inbox'},
-    {key: 'TODAY', name: 'Today'}
-    {key: 'NEXT 7 DAYS', name: 'Next 7 days'}
-]
+  { key: "INBOX", name: "Inbox" },
+  { key: "TODAY", name: "Today" },
+  { key: "NEXT 7 DAYS", name: "Next 7 days" },
+];
